d8fd3c18244696c4dbb1ab313f794197a6629695
Erik Kvale <eirikval@gmail.com>
2018-05-30 22:28:35 -0500

    Changed project name to match repo name

--- django-social-media-platform/urls.py
+++ django-social-media-platform/urls.py
@@ -1,4 +1,4 @@
-"""social_media_clone URL Configuration
+"""django-social-media-platform URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
