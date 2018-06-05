6f8694c846c1e5f98a3b9b2d549e7f3c630fa44c
Erik Kvale <eirikval@gmail.com>
2018-06-05 14:48:01 -0500

    Added to admin site

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+# Allows parent-child model view and edit on same page
+class GroupMemberInLine(admin.TabularInline):
+    model = models.GroupMember
+
+admin.site.register(models.Group)
