59668131e61cb07a15012e3c3dc97b315dd9bbe4
Erik Kvale <eirikval@gmail.com>
2018-06-07 14:11:32 -0500

    Added urls and connected to views in posts app

--- posts/views.py
+++ posts/views.py
@@ -40,6 +40,7 @@ class UserPosts(generic.ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['post_user'] = self.post_user
+        return context
 
 
 class PostDetail(SelectRelatedMixin, generic.DetailView):
