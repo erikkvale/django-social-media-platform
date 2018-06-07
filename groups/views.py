0fc806c1616336e402f7e69b12e95dd772a13dbf
Erik Kvale <eirikval@gmail.com>
2018-06-07 18:13:54 -0500

    Added additional views to groups

--- groups/views.py
+++ groups/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin,
@@ -6,6 +6,7 @@ from django.contrib.auth.mixins import (
 from django.urls import reverse
 from django.views import generic
 from groups.models import Group, GroupMember
+from django.contrib import messages
 
 
 class CreateGroup(LoginRequiredMixin, generic.CreateView):
@@ -24,8 +25,38 @@ class ListGroups(generic.ListView):
     model = Group
 
 
+class JoinGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single', kwargs={'slug': self.kwargs.get('slug')})
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get('slug'))
+        try:
+            GroupMember.objects.create(user=self.request.user, group=group)
+        except:
+            messages.warning(self.request, 'Warning already a member!')
+        else:
+            messages.success(self.request, 'You are now a member!')
 
 
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single', kwargs={'slug': self.kwargs.get('slug')})
+
+    def get(self, request, *args, **kwargs):
+        try:
+            membership = GroupMember.objects.filter(
+                user=self.request.user,
+                group__slug=self.kwargs.get('slug')
+            ).get()
+        except GroupMember.DoesNotExist:
+            messages.warning(self.request, 'Sorry you are not in the group!')
+        else:
+            membership.delete()
+            messages.success(self.request, 'You have left the group')
+        return super().get(request, *args, **kwargs)
 
 
 
