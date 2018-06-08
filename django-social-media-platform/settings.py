2183243d5f8b0e5a80d65fabadb76f307d507202
Erik Kvale <eirikval@gmail.com>
2018-06-08 12:12:57 -0500

    Added static root and adjusted posts btn bootstrap class, js and css master still not loading

--- django-social-media-platform/settings.py
+++ django-social-media-platform/settings.py
@@ -121,9 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
