9efc3b69c11b5a3568c2586c684e01c16dc07e1d
Erik Kvale <eirikval@gmail.com>
2018-06-08 17:27:56 -0500

    Massive rename

--- simple_social/urls.py
+++ simple_social/urls.py
@@ -1,4 +1,4 @@
-"""django-social-media-platform URL Configuration
+"""simple_social URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
